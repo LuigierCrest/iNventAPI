03c006927314512a890c0196e83b7c0c9ce807db
luis <28572673+LuigierCrest@users.noreply.github.com>
2026-01-08 22:13:51 +0000

    Rutas listas. Faltan algunas rutas más específicas para usos concretos de la APP y el login.

--- server/src/main/kotlin/com/luigiercrest/inventapi/models/entities/IncidenciaEntity.kt
+++ server/src/main/kotlin/com/luigiercrest/inventapi/models/entities/IncidenciaEntity.kt
@@ -6,7 +6,6 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import java.time.LocalDate
 
 
 class IncidenciaEntity(id: EntityID<Int>) : IntEntity(id)  {
